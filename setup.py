4603a147e7c1d79f59cb738d815d2db1e38c19dd
Doro Wu <dorowu@qnap.com>
2017-09-27 16:44:47 +0800

    pip install

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ Author: by wang_yang1980@hotmail.com
 from distutils.core import setup
 
 from os.path import abspath, dirname, join
-with open(join(dirname(abspath(__file__)), 'target', 'src', 'SikuliLibrary', 'version.py')) as f:
+with open(join(dirname(abspath(__file__)), 'target', 'src', 'version.py')) as f:
     exec(f.read())
 
 
